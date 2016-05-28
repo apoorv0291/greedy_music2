2fa21e30ba936c33feebd8a7ecdb2cc3361b127a
Apoorv Gupta <apoorv0291@gmail.com>
2016-05-28 23:32:15 +0530

    adding missing urls.py

--- greedy/greedy_music/urls.py
+++ greedy/greedy_music/urls.py
@@ -9,23 +9,17 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^search/?$', search_tracks), #search query
     #tracks url
-    url(r'^track/?$', view_all_music_tracks),
-    url(r'^track/(?P<track_id>\d+)/?$', track_details),
+    url(r'^track/?$', view_all_create_music_tracks),
+    url(r'^track/(?P<track_id>\d+)/?$', track_details_edit),
     #genre url
-    url(r'^genre/?$', view_all_genre),
-    url(r'^genre/(?P<genre_id>\d+)/?$', genre_details),
+    url(r'^genre/?$', view_all_create_genre),
+    url(r'^genre/(?P<genre_id>\d+)/?$', genre_details_edit),
     #user urls
     url(r'^register/?$', register_user),
     url(r'^login/?$', login_user),
     url(r'^logout/?$', logout_user),
-    url(r'^(?s).*$',view_all_music_tracks), #fallback url
-    url(r'^$', view_all_music_tracks),#fallback url
-    # url(r'^addGenre/?$', add_genre),
-    # url(r'^addTrack/?$', add_track),
-    # url(r'^editGenre/?$', edit_genre),
-    # url(r'^editTrack/?$', edit_track),
-
-
+    url(r'^(?s).*$',view_all_create_music_tracks), #fallback url
+    url(r'^$', view_all_create_music_tracks),#fallback url
     # url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
     #     'document_root': settings.MEDIA_ROOT}),
 
